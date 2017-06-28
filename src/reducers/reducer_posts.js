6683654d7e3a283ee05129d49860e20f33ba2615
vidurar <vidura.rajapaksa@gmail.com>
2017-06-29 01:28:47 +0530

    reducer updated

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,11 @@
-import {FETCH_POSTS, FETCH_POST} from '../actions/index';
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions/index';
 import _ from 'lodash';
 
 
 export default function(state={}, action){
   switch(action.type){
+    case DELETE_POST:
+      return _.omit(state, action.payload);
     case FETCH_POST:
     return {...state, [action.payload.data.id]:action.payload.data}
     case FETCH_POSTS:
